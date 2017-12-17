a45cf328ba8b0e30bfe32320d9e640cf40de9686
Steven Reiz <steven@reiz.nl>
2017-12-17 22:56:52 +0100

    updated to support playing against stockfish with cutechess-cli

--- uci.py
+++ uci.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pypy -u
+#!/usr/bin/pypy -u
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
@@ -15,6 +15,15 @@ from tools import WHITE, BLACK
 from xboard import Unbuffered, sunfish, input
 sys.stdout = Unbuffered(sys.stdout)
 
+logf=None
+
+def eprint(*args, **kwargs):
+    global logf
+    if logf==None:
+	logf=open("logf.txt", "w")
+    print(*args, file=logf, **kwargs)
+    logf.flush()
+
 def main():
     pos = tools.parseFEN(tools.FEN_INITIAL)
     searcher = sunfish.Searcher()
@@ -26,23 +35,26 @@ def main():
     # print name of chess engine
     print('Sunfish')
 
-    stack = []
     while True:
-        if stack:
-            smove = stack.pop()
-        else: smove = input()
+        smove = input()
 
-        if smove == 'quit':
+        if smove == 'quit' or smove=='stop':
             break
 
         elif smove == 'uci':
+	    print('id name Sunfish')
             print('uciok')
 
         elif smove == 'isready':
             print('readyok')
 
         elif smove == 'ucinewgame':
-            stack.append('position fen ' + tools.FEN_INITIAL)
+            pos=tools.parseFEN(tools.FEN_INITIAL)
+	    searcher = sunfish.Searcher()
+            forced = False
+            color = WHITE
+            our_time, opp_time = 1000, 1000 # time in centi-seconds
+            show_thinking = True
 
         elif smove.startswith('position'):
             params = smove.split(' ', 2)
@@ -50,11 +62,32 @@ def main():
                 fen = params[2]
                 pos = tools.parseFEN(fen)
                 color = WHITE if fen.split()[1] == 'w' else BLACK
+	    else:
+		params = smove.split(' ')
+		if len(params) == 1: continue
+		i = 1
+                while i < len(params):
+                    param = params[i]
+                    if param == 'startpos':
+		    	pos=tools.parseFEN(tools.FEN_INITIAL)
+			color = WHITE
+                    elif param == 'moves':
+                        i += 1
+			# rest are moves
+			while i < len(params):
+			    m0=params[i]
+			    m = tools.mparse(color, m0)
+		            pos = pos.move(m)
+		            color = 1-color
+			    i+=1
+                    i += 1
 
         elif smove.startswith('go'):
             #  default options
             depth = 1000
             movetime = -1
+            moves_remain = 40
+	    tc=0
 
             # parse parameters
             params = smove.split(' ')
@@ -66,14 +99,27 @@ def main():
                 if param == 'depth':
                     i += 1
                     depth = int(params[i])
-                if param == 'movetime':
+                elif param == 'movetime':
                     i += 1
                     movetime = int(params[i])
+		elif param == 'wtime':
+                    i += 1
+                    if color==WHITE:
+			movetime = int(params[i])
+		    tc=1
+		elif param == 'btime':
+                    i += 1
+                    if color==BLACK:
+                        movetime = int(params[i])
+		    tc=1
+		elif param == 'movestogo':
+                    i += 1
+		    moves_remain = int(params[i])
                 i += 1
 
+	    if tc==1 and moves_remain>0:
+		movetime = movetime / moves_remain
             forced = False
-
-            moves_remain = 40
 
             start = time.time()
             ponder = None
@@ -98,15 +144,11 @@ def main():
 
             entry = searcher.tp_score.get((pos, searcher.depth, True))
             m, s = searcher.tp_move.get(pos), entry.lower
-            # We only resign once we are mated.. That's never?
-            if s == -sunfish.MATE_UPPER:
-                print('resign')
+            moves = moves.split(' ')
+            if len(moves) > 1:
+                print('bestmove ' + moves[0] + ' ponder ' + moves[1])
             else:
-                moves = moves.split(' ')
-                if len(moves) > 1:
-                    print('bestmove ' + moves[0] + ' ponder ' + moves[1])
-                else:
-                    print('bestmove ' + moves[0])
+                print('bestmove ' + moves[0])
 
         elif smove.startswith('time'):
             our_time = int(smove.split()[1])
